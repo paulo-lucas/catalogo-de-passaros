d78a72a5a6f5f38c3e972a8ca0c582a6d6d21908
Paulo Lucas Cerqueira <paulolucas.2010@hotmail.com>
2020-03-20 23:56:10 -0300

    Menu finalizado

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,38 @@
+var passaros = getData();
+var menu = document.querySelector("#menu");
+
+//setta evento de transição dos botões
 var check = document.querySelector("#check");
-var opcoes = document.querySelectorAll(".botao-opcao");
-check.addEventListener("click", function(){
+check.addEventListener("click", fadeInOutBotoes);
+
+//itera menu
+passaros.forEach(function(passaro){
+    var botao = criaBotao();
+    var div = criaDiv(passaro.nome);
+    botao.appendChild(div);
+    menu.appendChild(botao);
+});
+
+//evento de transição dos botões
+function fadeInOutBotoes(){
+    var opcoes = document.querySelectorAll(".botao-opcao");
     var i = 0;
     var id = setInterval(function(){
         opcoes[i].classList.toggle("botao-visivel");
         i++;
         if(i == opcoes.length){ clearInterval(id); }
     }, 50);
-});
+}
+
+function criaBotao(){
+    var botao = document.createElement("button");
+    botao.classList.add("botao-opcao");
+    return botao;
+}
+
+function criaDiv(nomePassaro){
+    var div = document.createElement("div");
+    div.classList.add("opcao");
+    div.innerHTML = nomePassaro;
+    return div;
+}
