5acd2f6bd30e13bb82259a3d444bb08ffbaa3f2e
Paulo Lucas Cerqueira <paulolucas.2010@hotmail.com>
2020-03-21 03:36:11 -0300

    Imagens adicionadas

--- js/conteudo.js
+++ js/conteudo.js
@@ -1,19 +1,24 @@
 // atribui o evento de carregar as informações de cada passaro ao respectivo botão 
 function adicionaEvento(passaro, i = 0){
     var botoes = document.querySelectorAll(".botao-opcao");
+    console.log(botoes[i]);
+    console.log(i)
     botoes[i].addEventListener("click", function(){
         carregaConteudo(passaro);
+        destacaBotao(i-1);
     });
     i++;
 }
 
 // exibe as informações do passaro na tela
 function carregaConteudo(passaro){
+    var img = document.querySelector("#imagem-passaro");
     var nome = document.querySelector("#nome-passaro");
     var nomeCientifico = document.querySelector("#nome-cientifico");
     var descobrimento = document.querySelector("#descobrimento");
     var infoParagrafo = document.querySelectorAll(".info-p");
 
+    img.src = passaro.src;
     nome.innerHTML = passaro.nome;
     nomeCientifico.innerHTML = passaro.nomeCientifico;
     descobrimento.innerHTML = passaro.descobrimento;
@@ -21,4 +26,11 @@ function carregaConteudo(passaro){
     infoParagrafo[1].innerHTML = passaro.descricao;
     infoParagrafo[2].innerHTML = passaro.habitat;
     infoParagrafo[3].innerHTML = passaro.alimentacao;
+}
+
+// destaca o botão selecionado e volta o selecionado anteriormente ao normal
+function destacaBotao(i){
+    var botoes = document.querySelectorAll(".botao-opcao");
+    botoes.forEach(function(botao){ botao.classList.remove("botao-selecionado") })
+    botoes[i].classList.add("botao-selecionado");
 }
--- js/data.js
+++ js/data.js
@@ -8,7 +8,8 @@ var passaros = [
         familia: "Thraupidae",
         descricao: " O macho desta espécie é azul-brilhante com a cara e a garganta negra. A fêmea é esverdeada, em tom brilhante nas costas e amarelado nas partes inferiores. Nos dois sexos, há uma série de riscas escuras na plumagem ventral.",
         habitat: "Voa em bandos a procura de alimentos, pousando geralmente nos galhos mais expostos de árvores e arbustos de frutas da época. Surge e desaparece sem que ainda tenha tido o comportamento migratório bem determinado. Parece freqüentar certas regiões somente nas épocas do amadurecimento de frutos dos quais se alimenta.",
-        alimentacao: "Alimenta-se de frutos e insetos."
+        alimentacao: "Alimenta-se de frutos e insetos.",
+        src: "img/sai-andorinha.png"
     },
     {
         id: 1,
@@ -18,7 +19,8 @@ var passaros = [
         familia: "Cuculidae",
         descricao: "Tem cerca de 38 cm – Possui corpo franzino e cauda comprida. Ele é branco-amarelado, com bico cor de laranja. Bico forte e curvo. Quando empoleira, arrebita a cauda e joga-a até as costas. Anda sempre em bandos. São aves extremamente sociáveis.",
         habitat: "Vivem em campos, lavouras e ambientes mais abertos. À noite, para se esquentar, juntam-se em filas apertadas ou aglomeram-se em bandos desordenados. Ocorre em quase todo o Brasil, exceto na região amazônica.",
-        alimentacao: "São essencialmente carnívoros, comendo diversos insetos. Predam também camundongos e filhotes de outras aves. Periodicamente comem frutas, bagas, coquinhos e sementes, sobretudo na época seca quando há escassez de artrópodes."
+        alimentacao: "São essencialmente carnívoros, comendo diversos insetos. Predam também camundongos e filhotes de outras aves. Periodicamente comem frutas, bagas, coquinhos e sementes, sobretudo na época seca quando há escassez de artrópodes.",
+        src: "img/anu-branco.png"
     },
     {
         id: 2,
@@ -28,7 +30,8 @@ var passaros = [
         familia: "Accipitridae",
         descricao: "Mede cerca de 36 cm. Seu corpo é quase todo marrom, com barras brancas horizontais na barriga e verticais no peito. Quando voa, mostra uma marca cor de ferrugem em cada uma das suas asas. Chama a atenção por sobrevoar em casais, até sobre cidades, batendo asas rapidamente e vocalizando alto.",
         habitat: " Campos, capoeira, áreas urbanas, bordas de rios e alagados. Vive em quase todo o Brasil.",
-        alimentacao: "Artrópodes, pequenos vertebrados e outras aves."
+        alimentacao: "Artrópodes, pequenos vertebrados e outras aves.",
+        src: "img/gaviao-carijo.png"
     },
     {
         id: 3,
@@ -38,7 +41,8 @@ var passaros = [
         familia: "Falconidae",
         descricao: "Mede cerca de 130 cm de envergadura. Tem bico alaranjado, com a ponta cinza. Seu corpo é quase todo marrom, sendo brancas suas bochechas, nuca e garganta. Tem uma barra branca na ponta de cada asa – você consegue ver isso quando está voando.",
         habitat: "Campos, pastagens, alagados e áreas urbanas. Vive no sudeste, nordeste e Brasil central.",
-        alimentacao: "Come de tudo! Desde grãos até pequenos vertebrados, além de carcaças de animais."
+        alimentacao: "Come de tudo! Desde grãos até pequenos vertebrados, além de carcaças de animais.",
+        src: "img/carcara.png"
     },
     {
         id: 4,
@@ -48,7 +52,8 @@ var passaros = [
         familia: "Falconidae",
         descricao: " Mede cerca de 40 cm. Tem dorso, asas e cauda marrons e coloração creme nas partes inferiores. Sua cabeça também é creme e tem uma listra preta próxima aos olhos. Tem também uma listra branca em cada asa que fica visível quando voa.",
         habitat: " Áreas abertas, pastagens, bordas de alagados e praias. Vive em todo o Brasil.",
-        alimentacao: "Parasitas externos de bovinos e equinos, invertebrados, pequenos vertebrados, carniça e frutos."
+        alimentacao: "Parasitas externos de bovinos e equinos, invertebrados, pequenos vertebrados, carniça e frutos.",
+        src: "img/carrapateiro.png"
     },
     {
         id: 5,
@@ -58,7 +63,8 @@ var passaros = [
         familia: "Falconidae",
         descricao: "Tem apenas 25 cm – é pequeno em relação aos outros gaviões. Têm duas listras verticais nas bochechas, asas acinzentadas e, por baixo, é branco com pintas negras. Possui dimorfismo sexual acentuado.",
         habitat: "Campos arborizados, áreas desmatadas, restinga e áreas urbanas. Vive em todo o Brasil.",
-        alimentacao: "Artrópodes e pequenos vertebrados, desde lagartos até aves."
+        alimentacao: "Artrópodes e pequenos vertebrados, desde lagartos até aves.",
+        src: "img/quiriquiri.png"
     }
 ];
 
