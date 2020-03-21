be820268321db37e631dff8c5fc9d2ab323c7520
Paulo Lucas Cerqueira <paulolucas.2010@hotmail.com>
2020-03-20 21:36:58 -0300

    Versão inicial do projeto

---
+++ js/data.js
@@ -0,0 +1,67 @@
+
+var passaros = [
+    {
+        id: 1,
+        nome: "Saí-andorinha",
+        nomeCientifico: "Tersina viridis",
+        descobrimento: "(Illiger, 1811)",
+        familia: "Thraupidae",
+        descricao: " O macho desta espécie é azul-brilhante com a cara e a garganta negra. A fêmea é esverdeada, em tom brilhante nas costas e amarelado nas partes inferiores. Nos dois sexos, há uma série de riscas escuras na plumagem ventral.",
+        habitat: "Voa em bandos a procura de alimentos, pousando geralmente nos galhos mais expostos de árvores e arbustos de frutas da época. Surge e desaparece sem que ainda tenha tido o comportamento migratório bem determinado. Parece freqüentar certas regiões somente nas épocas do amadurecimento de frutos dos quais se alimenta.",
+        alimentacao: "Alimenta-se de frutos e insetos."
+    },
+    {
+        id: 2,
+        nome: "Anu-branco",
+        nomeCientifico: "Guira guira",
+        descobrimento: "(Gmelin, 1788)",
+        familia: "Cuculidae",
+        descricao: "Tem cerca de 38 cm – Possui corpo franzino e cauda comprida. Ele é branco-amarelado, com bico cor de laranja. Bico forte e curvo. Quando empoleira, arrebita a cauda e joga-a até as costas. Anda sempre em bandos. São aves extremamente sociáveis.",
+        habitat: "Vivem em campos, lavouras e ambientes mais abertos. À noite, para se esquentar, juntam-se em filas apertadas ou aglomeram-se em bandos desordenados. Ocorre em quase todo o Brasil, exceto na região amazônica.",
+        alimentacao: "São essencialmente carnívoros, comendo diversos insetos. Predam também camundongos e filhotes de outras aves. Periodicamente comem frutas, bagas, coquinhos e sementes, sobretudo na época seca quando há escassez de artrópodes."
+    },
+    {
+        id: 3,
+        nome: "Gavião-carijó",
+        nomeCientifico: "Rupornis magnirostris",
+        descobrimento: "(Gmelin, 1788)",
+        familia: "Accipitridae",
+        descricao: "Mede cerca de 36 cm. Seu corpo é quase todo marrom, com barras brancas horizontais na barriga e verticais no peito. Quando voa, mostra uma marca cor de ferrugem em cada uma das suas asas. Chama a atenção por sobrevoar em casais, até sobre cidades, batendo asas rapidamente e vocalizando alto.",
+        habitat: " Campos, capoeira, áreas urbanas, bordas de rios e alagados. Vive em quase todo o Brasil.",
+        alimentacao: "Artrópodes, pequenos vertebrados e outras aves."
+    },
+    {
+        id: 4,
+        nome: "Carcará",
+        nomeCientifico: "Caracara plancus",
+        descobrimento: "(Miller, 1777)",
+        familia: "Falconidae",
+        descricao: "Mede cerca de 130 cm de envergadura. Tem bico alaranjado, com a ponta cinza. Seu corpo é quase todo marrom, sendo brancas suas bochechas, nuca e garganta. Tem uma barra branca na ponta de cada asa – você consegue ver isso quando está voando.",
+        habitat: "Campos, pastagens, alagados e áreas urbanas. Vive no sudeste, nordeste e Brasil central.",
+        alimentacao: "Come de tudo! Desde grãos até pequenos vertebrados, além de carcaças de animais."
+    },
+    {
+        id: 5,
+        nome: "Carrapateiro",
+        nomeCientifico: "Milvago chimachima",
+        descobrimento: "(Vieillot, 1816)",
+        familia: "Falconidae",
+        descricao: " Mede cerca de 40 cm. Tem dorso, asas e cauda marrons e coloração creme nas partes inferiores. Sua cabeça também é creme e tem uma listra preta próxima aos olhos. Tem também uma listra branca em cada asa que fica visível quando voa.",
+        habitat: " Áreas abertas, pastagens, bordas de alagados e praias. Vive em todo o Brasil.",
+        alimentacao: "Parasitas externos de bovinos e equinos, invertebrados, pequenos vertebrados, carniça e frutos."
+    },
+    {
+        id: 6,
+        nome: "Quiriquiri",
+        nomeCientifico: "Falco sparverius",
+        descobrimento: "(Linnaeus, 1758)",
+        familia: "Falconidae",
+        descricao: "Tem apenas 25 cm – é pequeno em relação aos outros gaviões. Têm duas listras verticais nas bochechas, asas acinzentadas e, por baixo, é branco com pintas negras. Possui dimorfismo sexual acentuado.",
+        habitat: "Campos arborizados, áreas desmatadas, restinga e áreas urbanas. Vive em todo o Brasil.",
+        alimentacao: "Artrópodes e pequenos vertebrados, desde lagartos até aves."
+    }
+];
+
+function getData(){
+    return passaros;
+}
